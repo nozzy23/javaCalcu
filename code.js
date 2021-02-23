737868aee64fc3d21987b6c42f4a6ee3f3144403
Oscar Nunez <noscar07@gmail.com>
2021-02-22 22:02:44 -0500

    listening to click from the page

--- code.js
+++ code.js
@@ -11,3 +11,25 @@ function updateDIsplay() {
 }
 
 updateDIsplay();
+
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener ('click', (event) => {
+    const {target} = event;
+    
+    if (!target.matches('button')) {
+        return;
+    }
+    if (target.classList.contains('operator')) {
+        console.log('operator', target.value);
+        return;
+    }
+    if (target.classList.contains('decimal')) {
+        console.log('decimal', target.value);
+        return;
+    }
+    if (target.classList.contains('all-clear')) {
+        console.log('clear', target.value);
+        return;
+    }
+    console.log('digit', target.value);
+});
